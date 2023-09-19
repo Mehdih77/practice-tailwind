22c7630934880404afab7df461e58a1c4b07e335
Mehdi <mehdi.havaei77@gmail.com>
2023-09-19 17:53:07 +0330

    add cn function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import { cn } from "@/utils/cn";
 import Image from "next/image";
 
-export default function Home() {
+export default function Home({ className }: { className: string }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="group mx-4 flex h-80 w-full min-w-[260px] flex-col gap-4 rounded-lg border-4 border-orange-700 bg-teal-500 hover:bg-teal-600 sm:w-[580px]">
@@ -18,7 +19,12 @@ export default function Home() {
             Hello World!
           </div>
         </div>
-        <div className="flex-1 px-3 text-sm leading-4 text-gray-600 first-letter:text-3xl first-letter:text-gray-800 group-hover:font-bold group-hover:text-yellow-500">
+        <div
+          className={cn(
+            "flex-1 px-3 text-sm leading-4 text-gray-600 first-letter:text-3xl first-letter:text-gray-800 group-hover:font-bold group-hover:text-yellow-500",
+            className,
+          )}
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sapiente
           minus reprehenderit recusandae suscipit dignissimos ex molestias
           quidem libero perspiciatis, ipsum ratione veritatis totam iusto
