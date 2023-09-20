3199aca2ac787eb3e1a432191e828383f8915aad
Mehdi <mehdi.havaei77@gmail.com>
2023-09-20 14:07:02 +0330

    main styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Card from "@/components/card/Card";
 // import Test from "@/components/test/Test";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Test className="text-red-700" /> */}
+    <main className="container mx-auto my-10 flex justify-center px-5">
       <Card />
     </main>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   {/* <Test className="text-red-700" /> */}
+    //   <Card />
+    // </main>
   );
 }
